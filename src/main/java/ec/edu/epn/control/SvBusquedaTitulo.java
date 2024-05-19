520e2133e5062f298b4fce6ded3cb495d5739342
Cristian-Zambrano <cristian.zambrano@epn.edu.ec>
2024-05-19 18:38:22 -0500

    Changes-Servlet: he cambiado el metodo doGet del SvBusquedaTitulo para que muestre los resultados posteriormente en el catalogo

--- src/main/java/ec/edu/epn/control/SvBusquedaTitulo.java
+++ src/main/java/ec/edu/epn/control/SvBusquedaTitulo.java
@@ -1,34 +1,31 @@
 package ec.edu.epn.control;
 
+import ec.edu.epn.modelo.entidad.Videojuego;
 import ec.edu.epn.modelo.persistencia.VideojuegoDAO;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "SvBusqueda", value = "/SvBusqueda")
-public class SvBusqueda extends HttpServlet {
+@WebServlet(name = "SvBusquedaTitulo", value = "/SvBusquedaTitulo")
+public class SvBusquedaTitulo extends HttpServlet {
     VideojuegoDAO videojuegoDAO = new VideojuegoDAO();
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String filtro = request.getParameter("tipoFiltro");
-        //videojuegoDAO.getVideojuegoByTitulo(filtro);
-
-
-        if ("titulo".equals(filtro)) {
-            String titulo = request.getParameter("titulo");
-            System.out.println(titulo);
-            videojuegoDAO.getVideojuegoByTitulo(filtro);
-        }
+        String tituloEntrante = request.getParameter("titulo");
+        List<Videojuego> videojuegos =  videojuegoDAO.getVideojuegoByTitulo(tituloEntrante);
+        HttpSession sesion = request.getSession();
+        sesion.setAttribute("videojuegos", videojuegos);
+        response.sendRedirect("catalogoFiltrado.jsp");
 
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
     }
 }
