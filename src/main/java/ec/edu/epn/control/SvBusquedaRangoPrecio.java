19363fc9b9db43f4440a4905049aa24950369bd5
charlie-rzu <carla.r.acevedo@hotmail.com>
2024-05-19 18:34:55 -0500

    Modified Servlet de busqueda por rango de precio

--- src/main/java/ec/edu/epn/control/SvBusquedaRangoPrecio.java
+++ src/main/java/ec/edu/epn/control/SvBusquedaRangoPrecio.java
@@ -1,13 +1,16 @@
 package ec.edu.epn.control;
 
+import ec.edu.epn.modelo.entidad.Videojuego;
 import ec.edu.epn.modelo.persistencia.VideojuegoDAO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "SvBusquedaRangoPrecio", value = "/SvBusquedaRangoPrecio")
 public class SvBusquedaRangoPrecio extends HttpServlet {
@@ -15,20 +18,32 @@ public class SvBusquedaRangoPrecio extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String filtro = request.getParameter("tipoFiltro");
-        //videojuegoDAO.getVideojuegoByTitulo(filtro);
+        String precioMinStr = request.getParameter("precio_min");
+        String precioMaxStr = request.getParameter("precio_max");
 
+        double precioMinimo = 0.0;
+        double precioMaximo = 0.0;
 
-        if ("titulo".equals(filtro)) {
-            String titulo = request.getParameter("titulo");
-            System.out.println(titulo);
-            videojuegoDAO.getVideojuegoByTitulo(filtro);
+        try {
+            if (precioMinStr != null && !precioMinStr.isEmpty()) {
+                precioMinimo = Double.parseDouble(precioMinStr);
+            }
+            if (precioMaxStr != null && !precioMaxStr.isEmpty()) {
+                precioMaximo = Double.parseDouble(precioMaxStr);
+            }
+        } catch (NumberFormatException e) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Parámetros de precio inválidos.");
+            return;
         }
 
-    }
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        List<Videojuego> videojuegos = videojuegoDAO.getVideojuegosByRangoDePrecio(precioMinimo, precioMaximo);
 
+
+        HttpSession session = request.getSession();
+
+        session.setAttribute("videojuegos", videojuegos);
+
+        response.sendRedirect("catalogoFiltrado.jsp");
     }
 }
