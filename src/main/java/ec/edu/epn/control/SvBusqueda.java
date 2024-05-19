0b80452d66e1e35a9c75438052360230e823368d
Cristian-Zambrano <cristian.zambrano@epn.edu.ec>
2024-05-18 23:23:09 -0500

    Added-class: he añadido la clase de control busqueda para su posterior implementación.

---
+++ src/main/java/ec/edu/epn/control/SvBusqueda.java
@@ -0,0 +1,23 @@
+package ec.edu.epn.control;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class SvBusqueda extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+}
